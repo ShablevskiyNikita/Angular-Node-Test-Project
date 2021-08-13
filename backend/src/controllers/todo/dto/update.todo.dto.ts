a65ed028d2307356a0497d396684cccd651ac4ef
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 14:59:15 +0300

    Simplified update todo dto

--- backend/src/controllers/todo/dto/update.todo.dto.ts
+++ backend/src/controllers/todo/dto/update.todo.dto.ts
@@ -1,26 +1,7 @@
-import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { CreateTodoDto } from './create.todo.dto';
 
-export class UpdateTodoDto {
-  @IsNotEmpty({
-    message: 'Require not empty title'
-  })
-  @IsString({
-    message: 'Title must be a String'
-  })
-  title: string;
-
-  @IsNotEmpty({
-    message: 'Require not empty finished'
-  })
-  @IsBoolean({
-    message: 'Finished must be Boolean'
-  })
-  finished: boolean;
-
+export class UpdateTodoDto extends CreateTodoDto {
   constructor(data?: UpdateTodoDto) {
-    if(data){
-      this.title = data.title;
-      this.finished = data.finished;
-    }
+    super(data);
   }
 }
