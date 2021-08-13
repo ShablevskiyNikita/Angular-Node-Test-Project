fd14f840b400a4d44145366079aadc469980e83c
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 12:43:21 +0300

    Create simple todeo layout, withoout css

--- frontend/src/app/containers/portal/todo-list/todo/todo.component.ts
+++ frontend/src/app/containers/portal/todo-list/todo/todo.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { TodoDto } from '@rest/todo/todo.dto';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: [ './todo.component.scss' ]
 })
-export class TodoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodoComponent {
+  @Input() todoData: TodoDto;
+  @Output() todoDataChange = new EventEmitter<TodoDto>();
 }
--- frontend/src/app/rest/todo/todo.dto.ts
+++ frontend/src/app/rest/todo/todo.dto.ts
@@ -5,11 +5,9 @@ export class TodoDto {
   create_date?: Date;
 
   constructor(data?: TodoDto) {
-    if (data) {
-      this._id = data?._id ? data._id : null;
-      this.title = data?.title ? data.title : '';
-      this.finished = data?.finished ? data.finished : false;
-      this.create_date = data?.create_date ? data.create_date : null;
-    }
+    this._id = data?._id ? data._id : null;
+    this.title = data?.title ? data.title : '';
+    this.finished = data?.finished ? data.finished : false;
+    this.create_date = data?.create_date ? new Date(data.create_date) : null;
   }
 }
