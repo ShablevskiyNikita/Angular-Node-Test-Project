50fe9ea6bb85592621ff13b47eeb358fa4b751da
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 09:54:39 +0300

    Added try catch to the all controller methods

--- backend/src/controllers/todo/todo.controller.ts
+++ backend/src/controllers/todo/todo.controller.ts
@@ -6,25 +6,47 @@ import { RequestModifiedBody } from '../../core/interfaces/requests/request.modi
 import { CreateTodoDto } from './dto/create.todo.dto';
 import { UpdateTodoDto } from './dto/update.todo.dto';
 
+import { InternalErrorException } from '../../core/classes/exceptions/internal.error.exception';
+
 class TodoController implements ICrudController {
   create(request: RequestModifiedBody<CreateTodoDto>, response: Response, next: NextFunction) {
-    
+    try {
+
+    } catch (error) {
+      next(new InternalErrorException('Create todo', error));
+    }
   }
-  
+
   getOne(request: Request, response: Response, next: NextFunction) {
-    
+    try {
+
+    } catch (error) {
+      next(new InternalErrorException('Getting todo', error));
+    }
   }
-  
+
   getAll(request: Request, response: Response, next: NextFunction) {
-    
+    try {
+
+    } catch (error) {
+      next(new InternalErrorException('Getting todos', error));
+    }
   }
-  
+
   update(request: RequestModifiedBody<UpdateTodoDto>, response: Response, next: NextFunction) {
-    
+    try {
+
+    } catch (error) {
+      next(new InternalErrorException('Updating todo', error));
+    }
   }
-  
+
   delete(request: Request, response: Response, next: NextFunction) {
-    
+    try {
+
+    } catch (error) {
+      next(new InternalErrorException('Deleting todo', error));
+    }
   }
 
 }
