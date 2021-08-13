51b1ec86093fd52a3fde182fe130b2346a619f1b
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 09:59:07 +0300

    Filled all todo controller methods

--- backend/src/controllers/todo/todo.controller.ts
+++ backend/src/controllers/todo/todo.controller.ts
@@ -8,42 +8,60 @@ import { UpdateTodoDto } from './dto/update.todo.dto';
 
 import { InternalErrorException } from '../../core/classes/exceptions/internal.error.exception';
 
-class TodoController implements ICrudController {
-  create(request: RequestModifiedBody<CreateTodoDto>, response: Response, next: NextFunction) {
-    try {
+import { TodoDaoInstance } from './todo.dao';
 
+class TodoController implements ICrudController {
+  async create(request: RequestModifiedBody<CreateTodoDto>, response: Response, next: NextFunction) {
+    try {
+      const todo = await TodoDaoInstance.create(request.body);
+      
+      response.send(todo);
     } catch (error) {
       next(new InternalErrorException('Create todo', error));
     }
   }
 
-  getOne(request: Request, response: Response, next: NextFunction) {
+  async getOne(request: Request, response: Response, next: NextFunction) {
     try {
+      const { id } = request.params;
+      const todo = await TodoDaoInstance.findById(id);
 
+      response.send(todo);
     } catch (error) {
       next(new InternalErrorException('Getting todo', error));
     }
   }
 
-  getAll(request: Request, response: Response, next: NextFunction) {
+  async getAll(request: Request, response: Response, next: NextFunction) {
     try {
+      const todos = await TodoDaoInstance.findAll();
 
+      response.send(todos);
     } catch (error) {
       next(new InternalErrorException('Getting todos', error));
     }
   }
 
-  update(request: RequestModifiedBody<UpdateTodoDto>, response: Response, next: NextFunction) {
+  async update(request: RequestModifiedBody<UpdateTodoDto>, response: Response, next: NextFunction) {
     try {
+      const { id } = request.params;
+      const todo = await TodoDaoInstance.updateById(id, request.body);
 
+      response.send(todo);
     } catch (error) {
       next(new InternalErrorException('Updating todo', error));
     }
   }
 
-  delete(request: Request, response: Response, next: NextFunction) {
+  async delete(request: Request, response: Response, next: NextFunction) {
     try {
+      const { id } = request.params;
+      await TodoDaoInstance.deleteById(id);
 
+      response.send({
+        status: 200,
+        message: 'Successfully deleted'
+      });
     } catch (error) {
       next(new InternalErrorException('Deleting todo', error));
     }
