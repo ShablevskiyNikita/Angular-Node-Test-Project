f409d5d2382851b5af5ee0b6849742f8f7ca4d96
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 10:13:51 +0300

    Configured todo router paths, added bonus todo(verifying)

--- backend/src/controllers/todo/todo.router.ts
+++ backend/src/controllers/todo/todo.router.ts
@@ -1,10 +1,45 @@
 import { CommonRoute } from '../../core/classes/common.route';
 
+import { TodoMiddlewareInstance } from './todo.middleware';
+import { TodoControllerInstance } from './todo.controller';
+
+import { validateDtoMiddleware } from '../../core/middleware/validate.dto.middleware';
+
+import { CreateTodoDto } from './dto/create.todo.dto';
+import { UpdateTodoDto } from './dto/update.todo.dto';
+
 export class TodoRouter extends CommonRoute {
   constructor() {
     super('/todo');
   }
 
   initializeRoutes() {
+    // TODO Verifying
+    this.router
+      .post(
+        `${this.path}/`,
+        validateDtoMiddleware(CreateTodoDto),
+        TodoMiddlewareInstance.castRequestBodyToDto(CreateTodoDto),
+        TodoControllerInstance.create)
+      .get(
+        `${this.path}/`,
+        TodoControllerInstance.getAll)
+      .get(
+        `${this.path}/:id`,
+        TodoMiddlewareInstance.checkIdValidity,
+        TodoMiddlewareInstance.checkTodoExisting,
+        TodoControllerInstance.getOne)
+      .put(
+        `${this.path}/:id`,
+        TodoMiddlewareInstance.checkIdValidity,
+        TodoMiddlewareInstance.checkTodoExisting,
+        validateDtoMiddleware(UpdateTodoDto),
+        TodoMiddlewareInstance.castRequestBodyToDto(UpdateTodoDto),)
+      .delete(
+        `${this.path}/:id`,
+        TodoMiddlewareInstance.checkIdValidity,
+        TodoMiddlewareInstance.checkTodoExisting,
+        TodoControllerInstance.delete)
+    ;
   }
 }
