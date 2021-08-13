211ada82affffa3d28b0d8cc3cdda3029b817442
Nikita Shablevskyi <chitbomb@gmail.com>
2021-08-13 11:54:37 +0300

    Created todo service all crud methods

--- frontend/src/app/rest/todo/todo.dto.ts
+++ frontend/src/app/rest/todo/todo.dto.ts
@@ -1,5 +1,15 @@
-export class TodoDto{
+export class TodoDto {
+  _id?: string;
   title: string;
   finished: boolean;
   create_date?: Date;
+
+  constructor(data?: TodoDto) {
+    if (data) {
+      this._id = data?._id ? data._id : null;
+      this.title = data?.title ? data.title : '';
+      this.finished = data?.finished ? data.finished : false;
+      this.create_date = data?.create_date ? data.create_date : null;
+    }
+  }
 }
--- frontend/src/app/rest/todo/todo.service.ts
+++ frontend/src/app/rest/todo/todo.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class TodoService {
+import { HttpService } from '@core/http/http.service';
+import { TodoDto } from './todo.dto';
+import { Observable } from 'rxjs';
 
-  constructor() { }
+@Injectable()
+export class TodoService {
+  path = 'api/todo';
+
+  constructor(private http: HttpService) {}
+
+  createTodo(data: TodoDto): Observable<TodoDto> {
+    return this.http.create<TodoDto>(`${this.path}`, data, TodoDto, {
+      200: 'Todo successfully created',
+      201: 'Todo successfully created',
+      400: 'Wrong todo data',
+      401: 'Need authorization'
+    });
+  }
+
+  getTodos(): Observable<TodoDto[]> {
+    return this.http.getAll<TodoDto>(`${this.path}`, {}, TodoDto);
+  }
+
+  updateDto(data: TodoDto): Observable<TodoDto> {
+    return this.http.update<TodoDto>(`${this.path}`, data, TodoDto, {
+      200: 'Todo successfully updated',
+      201: 'Todo successfully updated',
+      400: 'Wrong todo data',
+      401: 'Need authorization'
+    });
+  }
+
+  deleteDto(data: TodoDto): Observable<TodoDto> {
+    return this.http.remove<TodoDto>(`${this.path}/${data._id}`, {}, {
+      200: 'Successfully deleted Todo',
+      201: 'Successfully deleted Todo',
+      404: 'Todo not found'
+    });
+  }
 }
